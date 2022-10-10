ef6c1323492d5a14d5353ed1ab684a096c772c67
Ogishi Gen <77260650+oogishigen@users.noreply.github.com>
2022-10-10 14:42:12 +0900

    Delete ddt_final_02 directory

--- ddt_final_02/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    document.getElementById("button").onclick = function () {
-        var text = document.getElementById("sendText").value;
-       location.href = "./test_name.html?data=" + encodeURIComponent(text);
-    }
-}
--- ddt_final_02/receive.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    var data = location.href.split("?")[1];
-    var text = data.split("=")[1];
-    document.getElementById("message").innerHTML = decodeURIComponent(text);
-    }
